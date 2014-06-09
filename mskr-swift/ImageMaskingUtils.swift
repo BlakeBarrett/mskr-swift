ef4456bd6700413f176ed6f5b4290a4b42368fa9
Blake Barrett <blake@blakebarrett.net>
2014-06-08 20:33:29 -0700

    This should all work. The only thing that doesn't is that the UIImage vars get dereferenced before they can be used by the static ImageMaskingUtils.

---
+++ mskr-swift/ImageMaskingUtils.swift
@@ -0,0 +1,93 @@
+//
+//  ImageMaskingUtils.swift
+//  mskr-swift
+//
+//  Created by Blake Barrett on 6/6/14.
+//  Copyright (c) 2014 Blake Barrett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageMaskingUtils {
+    
+    /**
+     * Masks the source image with the second.
+     */
+    class func maskImage(#source: UIImage, maskImage: UIImage) -> UIImage {
+        var maskRef: CGImageRef = maskImage.CGImage;
+        var mask: CGImageRef = CGImageMaskCreate(CGImageGetWidth(maskRef),
+            CGImageGetHeight(maskRef),
+            CGImageGetBitsPerComponent(maskRef),
+            CGImageGetBitsPerPixel(maskRef),
+            CGImageGetBytesPerRow(maskRef),
+            CGImageGetDataProvider(maskRef), nil, true);
+        
+        var masked: CGImageRef = CGImageCreateWithMask(source.CGImage, mask);
+        return  UIImage(CGImage: masked);
+    }
+    
+    /**
+     * Flattens or rasterizes two images into one.
+     */
+    class func mergeImages(#first: UIImage, second: UIImage) -> UIImage {
+        
+        let newImageSize: CGSize = CGSizeMake(
+            max(first.size.width, second.size.width),
+            max(first.size.height, second.size.height));
+        
+        if (UIGraphicsBeginImageContextWithOptions != nil) {
+            UIGraphicsBeginImageContextWithOptions(newImageSize, false, UIScreen().scale);
+        } else {
+            UIGraphicsBeginImageContext(newImageSize);
+        }
+        
+        let TWO: CGFloat = 2;
+        var wid: CGFloat = roundf(
+            (newImageSize.width - first.size.width) / TWO);
+        var hei: CGFloat = roundf(
+            (newImageSize.height-first.size.height) / TWO);
+        let firstPoint = CGPointMake(wid, hei);
+        first.drawAtPoint(firstPoint);
+        
+        wid = roundf(
+            (newImageSize.width - second.size.width) / TWO);
+        hei = roundf(
+            (newImageSize.height-second.size.height) / TWO);
+        let secondPoint = CGPointMake(wid, hei);
+        second.drawAtPoint(secondPoint);
+        
+        var image: UIImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return image;
+    }
+    
+    /**
+     * Returns a UIImage with the alpha modified
+     */
+    class func image(#fromImage: UIImage, withAlpha alpha: CGFloat) -> UIImage {
+        
+        var size: CGSize = fromImage.size;
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0);
+        
+        var ctx: CGContextRef = UIGraphicsGetCurrentContext();
+        var area: CGRect = CGRectMake(0, 0, size.width, size.height);
+        
+        CGContextScaleCTM(ctx, 1, -1);
+        CGContextTranslateCTM(ctx, 0, -area.size.height);
+        
+        CGContextSetBlendMode(ctx, kCGBlendModeMultiply);
+        
+        CGContextSetAlpha(ctx, alpha);
+        
+        CGContextDrawImage(ctx, area, fromImage.CGImage);
+        
+        var newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+        return newImage;
+    }
+}
--- mskr-swift/ViewController.swift
+++ mskr-swift/ViewController.swift
@@ -8,18 +8,123 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var imagePicker: UIImagePickerController = UIImagePickerController();
+    
+    var selectedImageInfoDict: NSDictionary = NSDictionary();
+    var selectedMask: String = "crclmsk";
+    let availableMasks = ["sqr", "crcl", "trngl", "POW", "plrd", "x", "eqlty", "hrt", "dmnd"];
+    
+    @IBOutlet var maskSelector : UIPickerView;
+    @IBOutlet var imageView: UIImageView;
+    @IBOutlet var backgroundImageView: UIImageView;
+    
+    var currentImage: UIImage = UIImage();
+    var backgroundImage: UIImage = UIImage();
+    var maskImage: UIImage = UIImage(named: "crclmsk");
+    
     override func viewDidLoad() {
+        imagePicker.delegate = self;
+        imagePicker.allowsEditing = true;
+        imagePicker.sourceType = .PhotoLibrary
+        // .PhotoLibrary, .Camera, .SavedPhotosAlbum
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary)
+        //TODO: File a bug that if I put the above line in [ ]  I crash the compiler
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onImageTouch(sender: AnyObject) {
+        presentViewController(imagePicker, animated: true) {}
+    }
 
-
+    
+    // MARK: UIPickerView goodies
+    // UIPickerViewDelegate "protocol" implementation
+    // returns the number of 'columns' to display.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return 1;
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int {
+        return availableMasks.count;
+    }
+    
+    // UIPickerViewDataSource "protocol" implementation
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
+        return availableMasks[row];
+    }
+    
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int) {
+        var maskName = availableMasks[row] + "msk";
+        self.maskImage = UIImage(named: maskName);
+        onImageSelected(image: self.currentImage, mask: self.maskImage);
+    }
+    
+    // MARK: UIImagePicker goodies
+    // UIImagePickerControllerDelegate interface/"protocol" implementation
+    
+    // handle camera capture
+    //func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!)
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!) {
+        println("Selected Image: \(info)");
+        selectedImageInfoDict = info;
+        
+        var selectedImage: UIImage = info.valueForKey("UIImagePickerControllerEditedImage") as UIImage;
+        onImageSelected(image: selectedImage, mask: self.maskImage);
+        
+        picker.dismissViewControllerAnimated(true){}
+        
+        // enable the mask selector
+        maskSelector.userInteractionEnabled = true;
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
+        picker.dismissViewControllerAnimated(true) {}
+    }
+    
+    // MARK: Mskr goodies
+    func onImageSelected(#image: UIImage!, mask: UIImage!) {
+        // foreground
+        self.currentImage = ImageMaskingUtils.maskImage(source: image, maskImage: mask);
+        imageView.image = currentImage;
+        
+        // background
+        self.backgroundImage = ImageMaskingUtils.image(fromImage: image, withAlpha: 0.5);
+        backgroundImageView.image = backgroundImage;
+    }
+    
+    @IBAction func onAddLayer(sender: AnyObject) {
+        currentImage = imageView.image;
+        var tempImage: UIImage = ImageMaskingUtils.mergeImages(first: backgroundImage, second: currentImage);
+        // TODO: Set alpha to .5
+        backgroundImage = ImageMaskingUtils.image(fromImage: tempImage, withAlpha: 0.5);
+        currentImage = tempImage;
+        imageView.image = tempImage;
+    }
+    
+    func onAddImage() {
+        
+    }
+    
+    @IBAction func onSave(sender: AnyObject) {
+        UIImageWriteToSavedPhotosAlbum(imageView.image,  nil, nil, nil);
+    }
+    
+    @IBAction func onStartOver(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onShare(sender: AnyObject) {
+        
+    }
 }
 
