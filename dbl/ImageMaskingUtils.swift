e215f8b31d38ac770dd5df2607d158c7a50e1134
Blake Barrett <blake@blakebarrett.net>
2016-03-18 01:59:43 -0700

    Initial commit! Kind-of works already.

---
+++ dbl/ImageMaskingUtils.swift
@@ -0,0 +1,224 @@
+//
+//  ImageMaskingUtils.swift
+//  mskr
+//
+//  Created by Blake Barrett on 6/6/14.
+//  Copyright © 2014 Blake Barrett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageMaskingUtils {
+    
+    class func colorControlImage(image: UIImage) -> UIImage {
+        let context = CIContext()
+        let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIColorControls")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        //Saturation: NSNumber/CIAttributeTypeScalar
+        //Brightness
+        //Contrast
+        
+        filter?.setValue(2.0, forKey: kCIInputContrastKey)
+        guard let result = filter?.valueForKey(kCIOutputImageKey) as? CIImage else {
+            return image
+        }
+        let ciImageRef = context.createCGImage(result, fromRect: result.extent)
+        let returnImage = UIImage(CGImage: ciImageRef)
+        return returnImage
+    }
+    
+    /**
+    * Changes the saturation of the image to the provided value
+    */
+    class func noirImage(image: UIImage!) -> UIImage {
+        let context = CIContext()
+        let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let result = filter?.valueForKey(kCIOutputImageKey) as? CIImage else {
+            return image
+        }
+        let ciImageRef = context.createCGImage(result, fromRect: result.extent)
+        let returnImage = UIImage(CGImage: ciImageRef)
+        return returnImage
+    }
+    
+    /**
+     * Changes the saturation of the image to the provided value
+     */
+    class func saturateImage(image: UIImage!, saturation: CGFloat) -> UIImage {
+        let context = CIContext()
+        let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIColorControls")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        filter?.setValue(saturation, forKey: kCIInputSaturationKey)
+        guard let result = filter?.valueForKey(kCIOutputImageKey) as? CIImage else {
+            return image
+        }
+        let ciImageRef = context.createCGImage(result, fromRect: result.extent)
+        let returnImage = UIImage(CGImage: ciImageRef)
+        return returnImage
+    }
+    
+    /**
+     * Invert colors of image
+     */
+    class func invertImageColors(image: UIImage) -> UIImage {
+        let context = CIContext()
+        let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIColorInvert")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let result = filter?.valueForKey(kCIOutputImageKey) as? CIImage else {
+            return image
+        }
+        let ciImageRef = context.createCGImage(result, fromRect: result.extent)
+        let returnImage = UIImage(CGImage: ciImageRef)
+        return returnImage
+    }
+    
+    /**
+     * Takes a greyscale image, darker the color the lower the alpha (0x000000 == 0.0)
+     */
+    class func imageToMask(image: UIImage) -> UIImage {
+        let context = CIContext()
+        let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIMaskToAlpha")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let result = filter?.valueForKey(kCIOutputImageKey) as? CIImage else {
+            return image
+        }
+        let ciImageRef = context.createCGImage(result, fromRect: result.extent)
+        let returnImage = UIImage(CGImage: ciImageRef)
+        return returnImage
+    }
+    
+    /**
+     * Masks the source image with the second.
+     */
+    class func maskImage(source: UIImage!, maskImage: UIImage!) -> UIImage {
+        let maskRef: CGImageRef! = maskImage.CGImage
+        let mask: CGImageRef! = CGImageMaskCreate(CGImageGetWidth(maskRef),
+            CGImageGetHeight(maskRef),
+            CGImageGetBitsPerComponent(maskRef),
+            CGImageGetBitsPerPixel(maskRef),
+            CGImageGetBytesPerRow(maskRef),
+            CGImageGetDataProvider(maskRef), nil, true)
+        
+        let sourceImage: CGImageRef! = source.CGImage
+        let masked: CGImageRef! = CGImageCreateWithMask(sourceImage, mask)
+        
+        let maskedImage = UIImage(CGImage: masked)
+        
+        return maskedImage
+    }
+    
+    /**
+     * Flattens or rasterizes two images into one.
+     */
+    class func mergeImages(first: UIImage, second: UIImage) -> UIImage {
+        
+        let newImageSize: CGSize = CGSizeMake(
+            max(first.size.width, second.size.width),
+            max(first.size.height, second.size.height))
+
+        UIGraphicsBeginImageContextWithOptions(newImageSize, false, 1)
+        
+        var wid: CGFloat = CGFloat(roundf(
+            CFloat(newImageSize.width - first.size.width) / 2.0))
+        var hei: CGFloat = CGFloat(roundf(
+            CFloat(newImageSize.height-first.size.height) / 2.0))
+        let firstPoint = CGPointMake(wid, hei)
+        first.drawAtPoint(firstPoint)
+        
+        wid = CGFloat(roundf(
+            CFloat(newImageSize.width - second.size.width) / 2.0))
+        hei = CGFloat(roundf(
+            CFloat(newImageSize.height-second.size.height) / 2.0))
+        let secondPoint = CGPointMake(wid, hei);
+        second.drawAtPoint(secondPoint)
+        
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
+    /**
+     * Returns a UIImage with the alpha modified
+     */
+    class func image(fromImage: UIImage, withAlpha alpha: CGFloat) -> UIImage {
+        return image(fromImage, withSize: fromImage.size, andAlpha: alpha);
+    }
+
+    class func image(fromImage: UIImage, withSize size:CGSize, andAlpha alpha: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1)
+        
+        let ctx: CGContextRef = UIGraphicsGetCurrentContext()!
+        let area: CGRect = CGRectMake(0, 0, size.width, size.height)
+        
+        CGContextScaleCTM(ctx, 1, -1)
+        CGContextTranslateCTM(ctx, 0, -area.size.height)
+        
+        CGContextSetBlendMode(ctx, CGBlendMode.Multiply)
+        
+        CGContextSetAlpha(ctx, alpha)
+        
+        CGContextDrawImage(ctx, area, fromImage.CGImage)
+        
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    /**
+     * Stretches images that aren't 1:1 to squares based on their longest edge
+     */
+    class func makeItSquare(image: UIImage) -> UIImage {
+        let longestSide = max(image.size.width, image.size.height)
+        let size: CGSize = CGSize(width: longestSide, height: longestSide)
+        
+        let x: CGFloat = (size.width - image.size.width) / 2
+        let y: CGFloat = (size.height - image.size.height) / 2
+        
+        let cropRect: CGRect = CGRectMake(x, y, size.width, size.height)
+        
+        let imageRef: CGImageRef = CGImageCreateWithImageInRect(image.CGImage!, cropRect)!
+        let cropped: UIImage = UIImage(CGImage: imageRef)
+        
+        return ImageMaskingUtils.image(cropped, withSize: size, andAlpha: 1)
+    }
+    
+    /**
+     * Takes an image and rotates it.
+     */
+    class func rotate(image: UIImage, radians: CGFloat) -> UIImage {
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox: UIView = UIView(frame: CGRectMake(0, 0, image.size.width, image.size.height));
+        let transform: CGAffineTransform = CGAffineTransformMakeRotation(radians);
+        rotatedViewBox.transform = transform;
+        let rotatedSize: CGSize = rotatedViewBox.frame.size;
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize);
+        let bitmap: CGContextRef = UIGraphicsGetCurrentContext()!
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+        
+        // Rotate the image context
+        CGContextRotateCTM(bitmap, radians);
+        
+        // Now, draw the rotated/scaled image into the context
+        CGContextScaleCTM(bitmap, 1.0, -1.0);
+        CGContextDrawImage(bitmap, CGRectMake(-image.size.width / 2, -image.size.height / 2, image.size.width, image.size.height), image.CGImage);
+        
+        let rotated: UIImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return rotated;
+    }
+}
--- dbl/ViewController.swift
+++ dbl/ViewController.swift
@@ -8,18 +8,122 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    /*
+    TODO:
+        1) Load image √
+        2) Desaturate image √
+        3) Apply desaturated image as alpha-mask
+        4) merge with any previous images in stack
+    */
+    
+    @IBOutlet weak var previewImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        // .PhotoLibrary, .Camera, .SavedPhotosAlbum
+        if let cameraType = UIImagePickerController.availableMediaTypesForSourceType(.Camera) {
+            imagePicker.mediaTypes = cameraType
+        } else {
+            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary)!
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Button click handlers
+    @IBAction func onAddButtonClick(sender: UIBarButtonItem) {
+        presentViewController(imagePicker, animated: true) { () -> Void in
+            // no-op
+        }
     }
-
+    @IBAction func onTrashButtonClicked(sender: UIBarButtonItem) {
+        self.previewImage.image = nil
+    }
+    
+    @IBAction func onActionButtonClicked(sender: UIBarButtonItem) {
+        UIGraphicsBeginImageContext(previewImage.bounds.size)
+        previewImage.image?.drawInRect(CGRect(x: 0, y: 0, width: previewImage.frame.size.width, height: previewImage.frame.size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        presentViewController(activity, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // ignore movies
+        if ("public.movie" == info[UIImagePickerControllerMediaType] as! NSString) {
+            return
+        }
+        
+        let image = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+        
+        self.previewImage.contentMode = .ScaleAspectFit
+        
+        // background thread
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) {
+            
+            // process image
+            var targetImage: UIImage
+            if (self.previewImage.image == nil) {
+                targetImage = image
+            } else {
+                // we have to do this assign/nil dance to free up as much memory as possible
+                var inverted: UIImage? = self.invertColorsForImage(ImageMaskingUtils.colorControlImage(image))
+                
+                var desaturated: UIImage? = ImageMaskingUtils.noirImage(inverted)
+                inverted = nil
+                
+                var mask: UIImage? = ImageMaskingUtils.imageToMask(desaturated!)
+                desaturated = nil
+                
+                var merged:UIImage? = self.maskImage(image, maskImage: mask!)
+                mask = nil
+                
+                targetImage = self.mergeImages(self.previewImage.image!, secondImage: merged!)
+                merged = nil
+            }
+            
+            // back to the main thread
+            dispatch_async(dispatch_get_main_queue(), {
+                self.previewImage.image = targetImage
+            })
+        }
+        
+        picker.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+    
+    // MARK: Image manipulations
+    func invertColorsForImage(image: UIImage) -> UIImage {
+        return ImageMaskingUtils.invertImageColors(image)
+    }
+    
+    func desaturateImage(image: UIImage) -> UIImage {
+        return ImageMaskingUtils.saturateImage(image, saturation: 0.0)
+    }
+    
+    func maskImage(image: UIImage, maskImage: UIImage) -> UIImage {
+        return ImageMaskingUtils.maskImage(image, maskImage: maskImage)
+    }
+    
+    func mergeImages(firstImage: UIImage, secondImage: UIImage) -> UIImage {
+        return ImageMaskingUtils.mergeImages(firstImage, second: secondImage)
+    }
 
 }
 
